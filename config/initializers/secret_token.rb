14f978b5aa0bed820ef918f570126554eacbbd90
Bonnie Varghese <bonnie.varghese@gmail.com>
2013-10-12 16:44:55 -0700

    Created status updates using rails scaffold status, rake db:migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '0ee5172bb2ed848a2e52187f7ccb9871ed4b652facf6eb3fe62bf84c72ce63cfb90670bff4a8298894c20dc081b05877882f9e1fa47ce1fd292bbf546dde81aa'
